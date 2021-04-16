2d8ccabd1c828aafa31b93668cb1fe2ea94bbc96
Hamza Toprak <hamzatoprak.tr@gmail.com>
2021-04-16 12:41:18 -0700

    Add view did layout subviews

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -21,5 +21,9 @@ class ViewController: UIViewController {
     override func viewDidDisappear(_ animated: Bool) {
         //
     }
+    
+    override func viewDidLayoutSubviews() {
+        //
+    }
 }
 
